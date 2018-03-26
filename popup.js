79eee1cd8791f99771b88dc906ec1b3165fd8468
mahabub01 <mahabubs4@gmail.com>
2018-03-27 00:49:51 +0600

    add notification for update manifest and popup file

--- popup.js
+++ popup.js
@@ -3,3 +3,16 @@
 $("#btn_click").on("click",function() {
 	$("#btn_result").text("hello");
 })
+
+
+$("#btn_notification").on("click",function() {
+	var notification = {
+		"type":"basic",
+		"iconUrl":"icon64.png",
+		"title":"notification title",
+		"message": "Hello user! it is not possible for u"
+	};
+
+
+	chrome.notifications.create("primaryNotification",notification);
+})
